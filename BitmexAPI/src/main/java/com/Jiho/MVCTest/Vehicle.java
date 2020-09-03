f6811782f6a881a95352050974ad3ce21d18b346
wlgh2626 <wlgh2626@hotmail.com>
2020-09-03 13:38:14 -0700

    Add files via upload

---
+++ BitmexAPI/src/main/java/com/Jiho/MVCTest/Vehicle.java
@@ -0,0 +1,19 @@
+package com.Jiho.MVCTest;
+
+public class Vehicle {
+	private int numTires;
+	private String brand;
+	
+	public int getNumTires() {
+		return numTires;
+	}
+	public void setNumTires(int numTires) {
+		this.numTires = numTires;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+}
---
+++ BitmexAPI/src/main/java/com/Jiho/MVCTest/data.java
@@ -0,0 +1,34 @@
+package com.Jiho.MVCTest;
+
+public class data {
+	
+	private String name;
+	private String version;
+	private long timeStamp;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setVersion(String version){
+		this.version = version;
+	}
+	
+	public String getVersion() {
+		return this.version;
+	}
+	
+	public void setTimeStamp(long timeStamp){
+		this.timeStamp = timeStamp;
+	}
+	
+	public long getTimeStamp() {
+		return this.timeStamp;
+	}
+
+	
+}
